cc851f9ffae257da909c7162db64b9ede2c13907
3rd48 <68355657+3rd48@users.noreply.github.com>
2020-10-24 12:56:27 +0800

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-# Script by : Thirdy
+# Script by : _Dreyannz_
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]=============================[][][]"
+echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m     AutoScriptVPS by Thirdy           "
+echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
 echo -e "\e[0m                                                   "
-read -p "   Username :  " User
+read -p "         Username       :  " User
 
 # Check If Username Exist, Else Proceed
 egrep "^$User" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]==============================[][][]"
+echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m     AutoScriptVPS by Thirdy           "
+echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
 echo -e "\e[0m                                                   "
-echo -e "\e[93m Username Already Exist              "
+echo -e "\e[93m              Username Already Exist              "
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]==============================[][][]\e[0m"
+echo -e "\e[94m[][][]======================================[][][]\e[0m"
 exit 0
 else
-read -p " Password:" Pass
-read -p " Active Days:" Days
+read -p "         Password       :  " Pass
+read -p "         Active Days    :  " Days
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]==============================[][][]\e[0m"
+echo -e "\e[94m[][][]======================================[][][]\e[0m"
 clear
 sleep 1
 MYIP=$(wget -qO- ipv4.icanhazip.com)
@@ -46,21 +46,21 @@ egrep "^$User" /etc/passwd >/dev/null
 echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
 clear
 echo -e "\e[0m                                                   "
-echo -e "\e[94m[][][]===============================[][][]"
+echo -e "\e[94m[][][]======================================[][][]"
 echo -e "\e[0m                                                   "
-echo -e "\e[93m     AutoScriptVPS by Thirdy         "
+echo -e "\e[93m           AutoScriptVPS by  _Dreyannz_           "
 echo -e "\e[0m                                                   "
-echo -e "      Username:$User"
-echo -e "      Password:$Pass"
-echo -e "      Expires on:$Expiration_Display"
+echo -e "         Username        :  $User"
+echo -e "         Password        :  $Pass"
+echo -e "         Expires on      :  $Expiration_Display"
 echo -e "\e[0m                                                   "	
-echo -e "      Host / IP:"$MYIP
-echo -e "      Port OpenSSH:"$opensshport
-echo -e "      Port Dropbear:"$dropbearport
-echo -e "      Port SSL:"$stunnel4port
-echo -e "      Port Squid:"$squidport
-echo -e "      Port OpenVPN:"$openvpnport
-echo -e "      $MYIP/client.ovpn"
+echo -e "         Host / IP       :  "$MYIP
+echo -e "         Port OpenSSH    :  "$opensshport
+echo -e "         Port Dropbear   :  "$dropbearport
+echo -e "         Port SSL        :  "$stunnel4port
+echo -e "         Port Squid      :  "$squidport
+echo -e "         Port OpenVPN    :  "$openvpnport
+echo -e "              $MYIP/client.ovpn"
 echo -e "                                                  "
-echo -e "\e[94m[][][]===============================[][][]\e[0m"
+echo -e "\e[94m[][][]======================================[][][]\e[0m"
 fi
