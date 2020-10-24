c0d664a05c9332f1b4c70d83025b7c78561068bd
3rd48 <68355657+3rd48@users.noreply.github.com>
2020-10-24 22:00:19 +0800

    Update and rename EZmod.sh to ocspanel.sh

--- ocspanel.sh
+++ ocspanel.sh
@@ -90,8 +90,8 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup 
 mv /etc/nginx/conf.d/vps.conf /etc/nginx/conf.d/vps.conf.backup 
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/wangzki03/VPSauto/master/nginx.conf" 
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/wangzki03/VPSauto/master/vps.conf" 
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/3rd48/thirdy/main/nginx.conf" 
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/3rd48/thirdy/main/vps.conf" 
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini 
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 
@@ -106,7 +106,7 @@ service nginx restart
 
 apt-get -y install zip unzip
 cd /home/vps/public_html
-wget https://raw.githubusercontent.com/wangzki03/VPSauto/master/EZ.zip
+wget https://raw.githubusercontent.com/3rd48/thirdy/main/EZ.zip
 unzip EZ.zip
 rm -f EZ.zip
 chown -R www-data:www-data /home/vps/public_html
@@ -172,7 +172,7 @@ clear
 echo "=======================================================" | tee -a log-install.txt
 echo "Please login Reseller Panel at http://$MYIP" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
-echo "Auto Script Installer OCS Panels EZ VPN Mod by Wangzki"  | tee -a log-install.txt
+echo "Auto Script Installer OCS Panels EZ VPN Mod by ThirdyMocky"  | tee -a log-install.txt
 
 echo "" | tee -a log-install.txt
 echo "Thanks " | tee -a log-install.txt
@@ -181,4 +181,4 @@ echo "Installation Log --> /root/log-install.txt" | tee -a log-install.txt
 echo "=======================================================" | tee -a log-install.txt
 cd ~/
 
-#rm -f /root/ocspanel.sh
+rm -f /root/ocspanel.sh
