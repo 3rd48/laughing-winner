38280091e99a776ea7a8f6a0c78a95c749151eac
3rd48 <68355657+3rd48@users.noreply.github.com>
2021-09-15 17:29:08 +0800

    Create thirdy.js

---
+++ local/updater/sockstunnel/thirdy.js
@@ -0,0 +1,7 @@
+{"version":"1", 
+"notes":"Latest Update Available.",
+"app_id":"ca-app-pub-8782705209245232~4634197623", 
+"banner":"ca-app-pub-3940256099942544/6300978111", 
+"interstitial":"ca-app-pub-3940256099942544/1033173712", 
+"Rewarded":"ca-app-pub-3940256099942544/5224354917"
+}
