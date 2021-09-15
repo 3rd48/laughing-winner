62e72ddb21c20f95c7dca7a5759d06df16316c39
3rd48 <68355657+3rd48@users.noreply.github.com>
2021-09-15 17:46:00 +0800

    Update thirdy.js

--- local/updater/sockstunnel/thirdy.js
+++ local/updater/sockstunnel/thirdy.js
@@ -2,6 +2,5 @@
 "notes":"Latest Update Available.",
 "app_id":"ca-app-pub-8782705209245232~4634197623", 
 "banner":"ca-app-pub-3940256099942544/6300978111", 
-"interstitial":"ca-app-pub-3940256099942544/1033173712", 
-"Rewarded":"ca-app-pub-3940256099942544/5224354917"
+"interstitial":"ca-app-pub-3940256099942544/1033173712"
 }
